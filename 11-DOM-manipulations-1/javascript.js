824f5e6ab0ad9b13199cfcf90f0441952259a6e6
Elias Torres <eliasomar.torres@gmail.com>
2022-07-24 13:20:47 -0700

    Adds blue and red text via js

--- 11-DOM-manipulations-1/javascript.js
+++ 11-DOM-manipulations-1/javascript.js
@@ -5,4 +5,16 @@ const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
+const paragraph = document.createElement('p');
+paragraph.classList.add('ptext');
+paragraph.textContent = "Hey I'm red!";
+paragraph.style.color = "red";
+
+const title = document.createElement('h3');
+title.classList.add('titleText');
+title.textContent = "I'm a blue h3";
+title.style.color = "blue";
+
 container.appendChild(content);
+container.appendChild(paragraph);
+container.appendChild(title);
